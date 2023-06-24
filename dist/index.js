244acc91d7f54eb40ddf3cfe0c9cd1cb37f1e34a
rl269 <rl269@student.waikato.ac.nz>
2023-06-24 17:08:22 +1200

    first commit

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [],
+        dbName: 'pernrl',
+        type: 'postgresql',
+        debug: !constants_1.__prod__
+    });
+};
+console.log("hello world hahaha ");
+//# sourceMappingURL=index.js.map
